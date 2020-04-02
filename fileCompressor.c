f6491e39980a054327690181551dcda4e0bbda45
fablessb <61425165+fablessb@users.noreply.github.com>
2020-04-02 17:53:57 -0400

    Add files via upload

--- fileCompressor.c
+++ fileCompressor.c
@@ -15,18 +15,10 @@ struct tokenNode{
 	int count;
 	struct tokenNode* next;
 };
-typedef struct minHeapNode {
-	char* token;
-	int count;
-} heapNode;
-typedef struct binaryTreeNode {
-	char* data;
-	treeNode* left;   
-	treeNode* right;
-} treeNode;
 struct tokenNode* countOccurrences(struct tokenNode* head, char* newToken){
 	if(head->token==NULL){
-		head->token=newToken;
+		head->token=malloc(strlen(newToken)+1);
+		strcpy(head->token, newToken);
 		head->count=1;
 		return head;
 	}
@@ -210,69 +202,229 @@ char* getToken(int file){
 		}
 	}
 	return NULL;
-	/*
-	struct tokenNode* tmp=firstTokenNode;
-	while(tmp!=NULL){
-		printf("%s: %d\n", tmp->token, tmp->count);
-		tmp=tmp->next;
-	}
-	*/
 }
-void compress(char *fileName, char* codeBookFile){
+int getIntLen(int num){
+	int a=1;
+	while(num>9){
+		a++;
+		num/=10;
+	}
+	return a;
+}
+struct tokenNode* parseCodebook(struct tokenNode* firstTokenNode, int codebook){
+	int spaceCheck=0, start=0, counter=1, num, i;
+	char* string;
+	while((string=getToken(codebook))!=NULL){
+		if(spaceCheck==0){
+			int stringLen=strlen(string);
+			char firstString[stringLen-1];
+			firstString[stringLen-2]='\0';
+			for(i=0;i<stringLen-2;i++)
+				firstString[i]=string[i];
+			if(start==0){
+				if(stringLen>2&&string[stringLen-1]=='t'){
+					start=1;
+					num=atoi(firstString);
+				}
+			}
+			else{
+				if(counter==0){
+					counter=1;
+					num=atoi(firstString);
+				}
+				else if(counter==1){
+					counter=0;
+					if(firstTokenNode->token==NULL){
+						firstTokenNode->token=malloc(strlen(firstString)+1);
+						strcpy(firstTokenNode->token, firstString);
+						firstTokenNode->count=num;
+					}
+					else{
+						//printf("%s\n", string);
+						struct tokenNode* tmp=firstTokenNode;
+						while(tmp->next!=NULL)
+							tmp=tmp->next;
+						struct tokenNode* newTokenNode=(struct tokenNode*)malloc(sizeof(struct tokenNode));
+						if(stringLen==1){
+							spaceCheck=1;
+							newTokenNode->token=malloc(stringLen+1);
+							newTokenNode->token=" ";
+						}
+						else{
+							newTokenNode->token=malloc(strlen(firstString)+1);
+							strcpy(newTokenNode->token, firstString);
+						}
+						newTokenNode->count=num;
+						newTokenNode->next=NULL;
+						tmp->next=newTokenNode;
+					}
+				}
+			}
+		}
+		else
+			spaceCheck=0;
+		//printf("%s\n", string);
+	}
+	return firstTokenNode;
+}
+void compress(char *fileName, char* codebookFile){
+	char* string;
+	int start=0, counter=1, num, spaceCheck=0, i;
 	int file=open(fileName, O_RDONLY);
-	int codeBook=open(fileName, O_RDONLY);
+	int codebook=open(codebookFile, O_RDONLY);
 	int newFile=creat(strcat(fileName, ".hcz"), S_IRWXG|S_IRWXO|S_IRWXU);
-	while(getToken(file)!=NULL){
-		printf("%s\n", getToken(file));
+	struct tokenNode* firstTokenNode=(struct tokenNode*)malloc(sizeof(struct tokenNode));
+	firstTokenNode=parseCodebook(firstTokenNode, codebook);
+	/*
+	struct tokenNode* tmp1=firstTokenNode;
+	while(tmp1!=NULL){
+		//printf("Token %s with code %d\n", tmp1->token, tmp1->count);
+		tmp1=tmp1->next;
+	}*/
+	while((string=getToken(file))!=NULL){
+		int stringLen=strlen(string), writeInt, countLen;
+		struct tokenNode* tmp=firstTokenNode;
+		if(stringLen==1){
+			while(tmp!=NULL){
+				if(strcmp(tmp->token, " ")==0){
+					countLen=getIntLen(tmp->count);
+					char buffer[countLen+1];
+					sprintf(buffer, "%d", tmp->count);
+					writeInt=write(newFile, buffer, countLen);
+				}
+				tmp=tmp->next;
+			}
+		}
+		else if(stringLen==2){
+			if(string[1]=='n'){
+				while(tmp!=NULL){
+					if(strcmp(tmp->token, "\\n")==0){
+						countLen=getIntLen(tmp->count);
+						char buffer[countLen+1];
+						sprintf(buffer, "%d", tmp->count);
+						writeInt=write(newFile, buffer, countLen);
+					}
+					tmp=tmp->next;
+				}
+			}
+			else if(string[1]=='t'){
+				while(tmp!=NULL){
+					if(strcmp(tmp->token, "\\t")==0){
+						countLen=getIntLen(tmp->count);
+						char buffer[countLen+1];
+						sprintf(buffer, "%d", tmp->count);
+						writeInt=write(newFile, buffer, countLen);
+					}
+					tmp=tmp->next;
+				}
+			}
+		}
+		else{
+			char firstString[stringLen-1], secondString[3];
+			firstString[stringLen-2]='\0';
+			secondString[2]='\0';
+			for(i=0;i<stringLen;i++){
+				if(i<stringLen-2)
+					firstString[i]=string[i];
+				else
+					secondString[stringLen-i-1]=string[i];
+			}
+			while(tmp!=NULL){
+				if(strcmp(tmp->token, firstString)==0){
+					countLen=getIntLen(tmp->count);
+					char buffer[countLen+1];
+					sprintf(buffer, "%d", tmp->count);
+					writeInt=write(newFile, buffer, countLen);
+				}
+				else if((secondString[0]==' '&&strcmp(tmp->token, " ")==0)||(secondString[0]=='n'&&strcmp(tmp->token, "\\n")==0)||(secondString[0]=='t'&&strcmp(tmp->token, "\\t")==0)){
+					countLen=getIntLen(tmp->count);
+					char buffer[countLen+1];
+					sprintf(buffer, "%d", tmp->count);
+					writeInt=write(newFile, buffer, countLen);
+				}
+				tmp=tmp->next;
+			}
+		}
+		//printf("%s\n", string);
 	}
 	close(file);
-	close(codeBook);
+	close(codebook);
 	close(newFile);
 }
-int isLeaf(treeNode* root) {
-	if (root == NULL) return 0;
-	if (root->left != NULL || root->right != NULL) return 0;
-	return 1;
-}
-int tokenNodeLength(struct tokenNode* front) {
-	struct tokenNode* ptr = front;
-	int length == 0;
-	while (ptr != NULL) {
-		ptr = ptr->next;
-		length++;
+void decompress(char* fileName, char* codebookFile){
+	//char codeStart[0];
+	int fileLen=strlen(fileName);
+	char newFileName[fileLen-3];
+	int start=0, count=1, num, spaceCheck=0, i, flag, writeInt;
+	char buffer;
+	newFileName[fileLen-4]='\0';
+	int file=open(fileName, O_RDONLY);
+	int codebook=open(codebookFile, O_RDONLY);
+	for(i=0;i<fileLen-4;i++)
+		newFileName[i]=fileName[i];
+	int newFile=creat(newFileName, S_IRWXG|S_IRWXO|S_IRWXU);
+	struct tokenNode* firstTokenNode=(struct tokenNode*)malloc(sizeof(struct tokenNode));
+	firstTokenNode=parseCodebook(firstTokenNode, codebook);
+	struct readNode* firstReadNode=(struct readNode*)malloc(sizeof(struct readNode));
+	firstReadNode->next=NULL;
+	while((flag=read(file, &buffer, sizeof(buffer)))>0){
+		count++;
+		char code[count];
+		//printf("%d\t%d\n", strlen(code), count);
+		code[count-1]='\0';
+		//printf("%d\n", strlen(code));
+		if(firstReadNode->c==0){
+			firstReadNode->c=buffer;
+			code[0]=buffer;
+			//printf("%s\n", code);
+		}
+		else{
+			int count2=0;
+			struct readNode* tmpReadNode=firstReadNode;
+			while(tmpReadNode->next!=NULL){
+				code[count2]=tmpReadNode->c;
+				count2++;
+				tmpReadNode=tmpReadNode->next;
+			}
+			code[count2]=tmpReadNode->c;
+			code[count2+1]=buffer;
+			struct readNode* newReadNode=(struct readNode*)malloc(sizeof(struct readNode));
+			newReadNode->c=buffer;
+			newReadNode->next=NULL;
+			tmpReadNode->next=newReadNode;
+		}
+		int codeNum=atoi(code);		
+		struct tokenNode* tmpTokenNode=firstTokenNode;
+		while(tmpTokenNode!=NULL){
+			if(tmpTokenNode->count==codeNum){
+				if(strcmp(tmpTokenNode->token, "\\n")==0)
+					writeInt=write(newFile, "\n", 1);
+				else if(strcmp(tmpTokenNode->token, "\\t")==0)
+					writeInt=write(newFile, "\t", 1);
+				else
+					writeInt=write(newFile, tmpTokenNode->token, strlen(tmpTokenNode->token));
+				count=1;
+				firstReadNode->c=0;
+				firstReadNode->next=NULL;
+				break;
+			}
+			tmpTokenNode=tmpTokenNode->next;
+		}
 	}
-	return length;
-}
-heapNode* makeMinHeap(struct tokenNode* list, int length) {
-	heapNode* heap = (heapNode* ) malloc(length * sizeof(heapNode));
-	if (length == 0 || heap == NULL || list == NULL) return NULL;
-	heap[0].token = (char* ) malloc( strlen(list[0].token) * sizeof(char) );
-	strcpy(heap[0].token, list[0].token);
-	heap[0].count = list[0].count;
-	if (length == 1) return heap;
-	int place = 1;
-	heapNode* current = heap[1];
-	struct tokenNode* ptr = list[1];
-	while (ptr != NULL) {
-		heap[place].token = (char* ) malloc( strlen(list[place].token) * sizeof(char) );
-		strcpy(heap[place].token, list[place].token);
-		heap[place].count = list[place].count;
-		siftUp(heap, place);
-		siftDown(heap, length);
-		place++;
-	}
-	return heap;
-}
-void siftUp(heapNode* heap, int place) {
 	
-}
-void siftDown(heapNode* heap, int length) {
-
+	
+	close(file);
+	close(codebook);
+	close(newFile);
 }
 int main(int argc, char **argv){
 	//going through directories recursively
 	//printFiles(argv[1]);
+	
 	//compress a file given a codebook
-	compress(argv[1], argv[2]);
+	//compress(argv[1], argv[2]);
+	
+	//decompress a file given a codebook
+	//decompress(argv[1], argv[2]);
 	return EXIT_SUCCESS;
 }
